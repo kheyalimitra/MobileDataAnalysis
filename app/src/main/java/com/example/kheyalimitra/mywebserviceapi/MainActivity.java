e8dd22e3c7032a2458f4a704bc193d3fd87cc225
Kheyali Mitra <kheyalimitra@gmail.com>
2015-03-27 22:20:01 -0400

    New code structure

--- app/src/main/java/com/example/kheyalimitra/mywebserviceapi/MainActivity.java
+++ app/src/main/java/com/example/kheyalimitra/mywebserviceapi/MainActivity.java
@@ -1,55 +1,39 @@
 package com.example.kheyalimitra.mywebserviceapi;
 
-import android.app.AlertDialog;
-import android.app.Service;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.app.Activity;
-import android.widget.ExpandableListView;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import com.unnamed.b.atv.model.TreeNode;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
+import com.unnamed.b.atv.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Iterator;
-
 import java.util.Map;
-import java.util.Set;
 
 
 public class MainActivity  extends ActionBarActivity { //extends Activity{
 
-    //Top level names of Domain
-    List<String> groupList;
-
+    public static Context MainContext;
     //Child hiearchy
     public Map<String, List<String>> AdventureWorksDomainDetails;
-
     //Measure details
-    public ArrayList<String> AdventureWorksMeasureDetails ;
-    public static Context MainContext;
+    public ArrayList<String> AdventureWorksMeasureDetails;
+    //Top level names of Domain
+    List<String> groupList;
     //List view object
     ExpandableListView expListView;
 
@@ -57,72 +41,55 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //final Button btn = (Button) findViewById(R.id.getbtn);
+
+        //List view entry for first screen
         final LinkedHashMap<String, Class<?>> listItems = new LinkedHashMap<>();
+        //First screen Output for List window
         listItems.put("Get Dimensions and Measures", DimensionTree.class);
 
-        //final TextView dimView = (TextView) findViewById(R.id.domainView);
-        //final TextView measureView = (TextView) findViewById(R.id.measureView);
-        //dimView.setVisibility(View.INVISIBLE);
-        //measureView.setVisibility(View.INVISIBLE);
         ///Display data in Text view and List View
         final AlertDialog ad = new AlertDialog.Builder(this).create();
         final Activity activityObj = this;
         MainContext = activityObj;
-        //PopulateDetails details =  new PopulateDetails();
 
-        //details.BindButtonClick(btn, expListAdapter, activityObj);
-
-
+        //Put list into string list
         final List<String> list = new ArrayList(listItems.keySet());
+
+        //Activity control in first screens ActivityMain.xml
         final ListView listview = (ListView) findViewById(R.id.listview);
+
+        //Initialize array Adpater
         final SimpleArrayAdapter adapter = new SimpleArrayAdapter(this, list);
+        //Sets Adapter
         listview.setAdapter(adapter);
+        //Onclick of List view
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        //btn.setOnClickListener(new View.OnClickListener() {
+                                            //btn.setOnClickListener(new View.OnClickListener() {
 
-                                   /**
-                                    * Populates JSON data into view
-                                    */
-                                   @Override
-                                  // public void onClick(View arg0) {
-                                   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                       try {
-
-                                           /*ParseJSONResponse parse = new ParseJSONResponse();
-                                           _startServiceThread();
-                                           AdventureWorksDomainDetails = parse.ParseDomainRecords(ServiceCallThread.Domains);
-                                           AdventureWorksMeasureDetails = parse.ParseMeasureResponse(ServiceCallThread.Measures);*/
-                                           //groupList = _populateTopLevelHierarchy();
-                                           ///parse webservice response and populate HashMap List
-                                          // _populateJSONResponseToUI(activityObj, btn);
-
-
-                                           ///// new code for tree view
-
-                                          // Class<?> clazz = listItems.values().toArray(new Class<?>[]{})[position];
-                                           Intent i = new Intent(MainActivity.this, TreeViewActivity.class);
-                                           //Intent androidView = new Intent(MainActivity.this,DimensionTree.class);
-                                           Class<?> clazz = listItems.values().toArray(new Class<?>[]{})[position];
-                                           i.putExtra(TreeViewActivity.View_PARAM,clazz );
-                                           MainActivity.this.startActivity(i);
-                                           //MainActivity.this.startActivity(androidView);
-                                           ////////
-                                      } catch (Exception e) {
-                                           String s = e.getMessage();
-                                       }
-
-                                   }
-                               }
+                                            /**
+                                             * Populates JSON data into view
+                                             */
+                                            @Override
+                                            // public void onClick(View arg0) {
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                try {
+                                                    Intent i = new Intent(MainActivity.this, TreeViewActivity.class);
+                                                    Class<?> clazz = listItems.values().toArray(new Class<?>[]{})[position];
+                                                    i.putExtra(TreeViewActivity.View_PARAM, clazz);
+                                                    MainActivity.this.startActivity(i);
+                                                } catch (Exception e) {
+                                                    String s = e.getMessage();
+                                                }
+                                            }
+                                        }
         );
 
     }
 
-
     /**
      * Start Service thread
      */
-    public void StartServiceThread(){
+    public void StartServiceThread() {
 
         //Initialize domain and measure as START
         //After valid population of both the variables, thread will come out of sleep mode
@@ -145,13 +112,13 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
         }
     }
 
-    /***
+    /**
      * Populates top level hierarchy of domain
+     *
      * @return
      */
     //private ArrayList<String> _populateTopLevelHierarchy()
-    public TreeNode PopulateTreeHierarchy()
-    {
+    public TreeNode PopulateTreeHierarchy() {
         TreeNode dRoot = new TreeNode("Dimension/Hierarchy:");
         TreeNode dimRoot = new TreeNode("");
 
@@ -167,7 +134,7 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
             while (it.hasNext()) {
                 Map.Entry pair = (Map.Entry) it.next();
                 TreeNode t = new TreeNode(pair.getKey());
-                if(AdventureWorksDomainDetails.get(pair.getKey()).size()>1) {
+                if (AdventureWorksDomainDetails.get(pair.getKey()).size() > 1) {
                     List<String> innerchild = (List<String>) pair.getValue();
                     Iterator inner = innerchild.iterator();
                     while (inner.hasNext()) {
@@ -180,17 +147,14 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
                 dRoot.addChild(t);
             }
 
-        }
-        catch (Exception e)
-        {
-                String s = e.getMessage();
+        } catch (Exception e) {
+            String s = e.getMessage();
         }
         dimRoot.addChild(dRoot);
-        return  dimRoot;
+        return dimRoot;
     }
 
-    public TreeNode PopulateMeasures()
-    {
+    public TreeNode PopulateMeasures() {
         TreeNode mRoot = new TreeNode("Measures:");
         TreeNode mesRoot = new TreeNode("");
         try {
@@ -208,8 +172,9 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
 
         }
         mesRoot.addChild(mRoot);
-        return  mesRoot;
+        return mesRoot;
     }
+
     /***
      * Displays the records in Expandable List adapter and List view
      * @param activityObj
@@ -220,12 +185,11 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
         //TextView errorView = (TextView) findViewById(R.id.ErrorText);
         //ListView mView = (ListView) findViewById(R.id.measurelistView);
         //ExpandableListView expListView = (ExpandableListView) findViewById(R.id.expandableListView);
-        try
-        {
+        try {
             ArrayAdapter<String> arrayAdapter =
-                    new ArrayAdapter<String>(activityObj,android.R.layout.simple_list_item_1, AdventureWorksMeasureDetails ); // mView.setAdapter(arrayAdapter);
-           // mView.setAdapter(arrayAdapter);
-            final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(activityObj,groupList,AdventureWorksDomainDetails);
+                    new ArrayAdapter<String>(activityObj, android.R.layout.simple_list_item_1, AdventureWorksMeasureDetails); // mView.setAdapter(arrayAdapter);
+            // mView.setAdapter(arrayAdapter);
+            final ExpandableListAdapter expListAdapter = new ExpandableListAdapter(activityObj, groupList, AdventureWorksDomainDetails);
             //TextView tView =  (TextView) findViewById(R.id.textView);
             btn.setVisibility(View.GONE);
             //errorView.setVisibility(View.GONE);
@@ -236,33 +200,30 @@ public class MainActivity  extends ActionBarActivity { //extends Activity{
                 public boolean onChildClick(ExpandableListView parent, View v,
                                             int groupPosition, int childPosition, long id) {
 
-                     String selectedChild = (String) expListAdapter.getChild(
+                    String selectedChild = (String) expListAdapter.getChild(
                             groupPosition, childPosition);
-                     String selectedGroup = (String) expListAdapter.getGroup(groupPosition);
+                    String selectedGroup = (String) expListAdapter.getGroup(groupPosition);
                     /*Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
                             .show();*/
-                    String hierarchy = selectedGroup+"."+selectedChild;
+                    String hierarchy = selectedGroup + "." + selectedChild;
                     try {
                         _startServiceThreadforHierarchy(hierarchy);
                         ParseJSONResponse parse = new ParseJSONResponse();
                         ArrayList<String> AdventureWorksHierarchyDetails = parse.ParseHierarchy(ServiceCallThread.Hierarchy);
                         //expListAdapter.setHierarchy(AdventureWorksHierarchyDetails);
 
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
 
                     }
                     return true;
                 }
             });
-        }
-        catch(Exception ex)
-        {
-           // errorView.setVisibility(View.VISIBLE);
+        } catch (Exception ex) {
+            // errorView.setVisibility(View.VISIBLE);
             //errorView.setText("Error!Please Try Again.");
         }
     }
+
 private  void _startServiceThreadforHierarchy(String param)
 {
      //Initialize domain and measure as START
@@ -273,7 +234,7 @@ private  void _startServiceThreadforHierarchy(String param)
     try {
         sthread.join();
         sthread.start();
-        while (ServiceCallThread.Hierarchy == "START" ) {
+        while (ServiceCallThread.Hierarchy == "START") {
             try {
                 Thread.sleep(10);
             } catch (Exception ex) {
@@ -284,6 +245,7 @@ private  void _startServiceThreadforHierarchy(String param)
 
     }
 }
+
     private void setGroupIndicatorToRight() {
         /* Get the screen width */
         DisplayMetrics dm = new DisplayMetrics();
@@ -293,13 +255,13 @@ private  void _startServiceThreadforHierarchy(String param)
         expListView.setIndicatorBounds(width - getDipsFromPixel(35), width
                 - getDipsFromPixel(5));
     }
+
     public int getDipsFromPixel(float pixels) {
         // Get the screen's density scale
         final float scale = getResources().getDisplayMetrics().density;
         // Convert the dps to pixels, based on density scale
         return (int) (pixels * scale + 0.5f);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -322,10 +284,11 @@ private  void _startServiceThreadforHierarchy(String param)
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Class for
     private class SimpleArrayAdapter extends ArrayAdapter<String> {
         public SimpleArrayAdapter(Context context, List<String> objects) {
             super(context, android.R.layout.simple_list_item_1, objects);
-
         }
 
         @Override
@@ -333,4 +296,5 @@ private  void _startServiceThreadforHierarchy(String param)
             return position;
         }
     }
+
 }
